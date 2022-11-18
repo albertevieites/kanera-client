cf989a2b28a053a7c4f1227177e68fc6cea20cc8
Alberte Vieites <albertevieites@gmail.com>
2022-11-18 18:21:16 +0000

    feat: :construction: wip expense form component

--- src/components/ExpenseForm/ExpenseForm.jsx
+++ src/components/ExpenseForm/ExpenseForm.jsx
@@ -1,53 +1,84 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const ExpenseForm = (props) => {
+const ExpenseForm = props => {
+	// variables
 	const navigate = useNavigate();
+	const num = 0;
+	// Format date variable
+	const todayDate = new Date().toISOString().slice(0, 10);;
 
-	const [name, setName] = useState(" ");
-	const [amount, setAmount] = useState(0);
+	// useState for input fields
+	const [form, setForm] = useState({
+		date: todayDate,
+		name: '',
+		amount: Number(num),
+	});
 
-	const handleNameChange = event => setName(event.target.value)
-	const handleAmountChange = event => setAmount(event.target.value)
-
-
-	const handleSubmit = async () => {
+	// Submit function 🚀 - Handle form submission
+	/* const handleSubmit = async () => {
 		const newExpense = {
+			date: date,
 			name: name,
 			amount: amount,
-		}
+		};
 
 		try {
-			await axios.post("http://localhost:5005/api/expenses", newExpense)
+			await axios.post('http://localhost:5005/api/expenses', newExpense);
 			props.getExpenses();
 		} catch (error) {
 			// ...
-			navigate("/error");
+			navigate('/error');
 		}
+	}; */
+
+	// Handle change events from input fields
+	const onChange = e => {
+		const { value, name } = e.target;
+
+		setForm(state => ({
+			...state,
+			[name]: value,
+		}));
 	};
 
+	// Show data on the console
+	const showData = () => {
+		console.log('Form: ', form);
+	};
+
+	// Prevent refreshing page
+	const onSubmit = e => {
+		e.preventDefault();
+
+		showData();
+	};
+
+	// HTML render
 	return (
 		<div className='expense--form'>
 			<h3>Add your expense</h3>
 
-			<form onSubmit={handleSubmit}>
-				<label>Name</label>
-				<input
-					type='text'
-					name='name'
-					value={name}
-					onChange={handleNameChange}
-				/>
+			<form onSubmit={onSubmit}>
+				{/* Date field */}
+				<label>Date</label>
+				<input type='date' name='date' value={form.date} onChange={onChange} />
 
+				{/* Name field */}
+				<label>Name</label>
+				<input type='text' name='name' value={form.name} onChange={onChange} />
+
+				{/* Amount field */}
 				<label>Amount</label>
 				<input
 					type='number'
 					name='amount'
-					value={amount}
-					onChange={handleAmountChange}
+					value={form.amount}
+					onChange={onChange}
 				/>
 
-				<button type='submit'>Fill it out!</button>
+				{/* Button to submit data */}
+				<button>Fill it out!</button>
 			</form>
 		</div>
 	);
