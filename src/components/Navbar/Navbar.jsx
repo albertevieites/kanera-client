e2b2a887606ea3e969034a140cc6e12eb3c326c8
Alberte Vieites <albertevieites@gmail.com>
2022-12-13 19:05:59 +0000

    feat: :sparkles: add authentication

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { useContext, useReducer } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+import { AuthContext } from '../../context/auth.context';
 
 const Navbar = () => {
-	return (
-		<div className='navbar'>
-			<Link to='/'>KANERA</Link>
-			<Link to='/income'>Income</Link>
-			<Link to='/expenses'>Expenses</Link>
-			<Link to='/budget'>Budget</Link>
-			<Link to='/profile'>Profile</Link>
-			<Link to='/signup'>Signup</Link>
-			<Link to='/login'>Login</Link>
-		</div>
-	);
+	const navigate = useNavigate();
+
+	const { isUserActive, user, authenticateUser } = useContext(AuthContext);
+
+	const handleLogout = () => {
+		// Destroy token
+		localStorage.removeItem("authToken");
+		// Authenticate user
+		authenticateUser();
+		// Redirect
+		navigate("/");
+	}
+
+	if (isUserActive === true) {
+		return (
+			<div className='navbar--active'>
+				<Link to='/'>KANERA</Link>
+				<Link to='/income'>Income</Link>
+				<Link to='/expenses'>Expenses</Link>
+				<Link to='/budget'>Budget</Link>
+				<Link to='/profile'>Profile</Link>
+				<button onClick={handleLogout}>Logout</button>
+				<p className='navbar--active__email'>{user.email}</p>
+			</div>
+		);
+	} else {
+		return (
+			<div className='navbar'>
+				<Link to='/signup'>Signup</Link>
+				<Link to='/login'>Login</Link>
+			</div>
+		);
+	}
 };
 
 export default Navbar;
