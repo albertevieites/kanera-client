3a64e35af2894d387212ea6963de183e8247098e
Alberte Vieites <albertevieites@gmail.com>
2022-12-23 18:00:37 +0000

    feat: :sparkles: add profile edit page

--- src/pages/Profile/ProfileEdit.jsx
+++ src/pages/Profile/ProfileEdit.jsx
@@ -1,25 +1,78 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import {
+	getProfileService,
+	updateProfileService,
+} from '../../services/profile.services';
+
+import arrCountry from '../../utils/country';
+import arrGender from '../../utils/gender';
 
 function ProfileEdit() {
-  const num = 0;
+	const { id } = useParams();
+	const navigate = useNavigate();
 
-  const [avatar, setAvatar] = useState("");
-  const [fullname, setFullname] = useState("");
-  const [email, setEmail] = useState("");
-  const [profession, setProfession] = useState("");
-  const [age, setAge] = useState(Number(num));
-  const [gender, setGender] = useState("");
-  const [city, setCity] = useState("");
-  const [country, setCountry] = useState("");
+	const num = 0;
 
-  const handleAvatarChange = (event) => setAvatar(event.target.value);
-  const handleFullnameChange = (event) => setFullname(event.target.value);
-  const handleEmailChange = (event) => setEmail(event.target.value);
-  const handleProfessionChange = (event) => setProfession(event.target.value);
-  const handleAgeChange = (event) => setAge(event.target.value);
-  const handleGenderChange = (event) => setGender(event.target.value);
-  const handleCityChange = (event) => setCity(event.target.value);
-  const handleCountryChange = (event) => setCountry(event.target.value);
+	const [avatar, setAvatar] = useState('');
+	const [fullname, setFullname] = useState('');
+	const [email, setEmail] = useState('');
+	const [profession, setProfession] = useState('');
+	const [age, setAge] = useState(Number(num));
+	const [gender, setGender] = useState('');
+	const [city, setCity] = useState('');
+	const [country, setCountry] = useState('');
+
+	const handleAvatarChange = (event) => setAvatar(event.target.files[0]);
+	const handleFullnameChange = (event) => setFullname(event.target.value);
+	const handleEmailChange = (event) => setEmail(event.target.value);
+	const handleProfessionChange = (event) => setProfession(event.target.value);
+	const handleAgeChange = (event) => setAge(event.target.value);
+	const handleGenderChange = (event) => setGender(event.target.value);
+	const handleCityChange = (event) => setCity(event.target.value);
+	const handleCountryChange = (event) => setCountry(event.target.value);
+
+	useEffect(() => {
+		getProfileDetails();
+	}, []);
+
+	const getProfileDetails = async () => {
+		try {
+			const response = await getProfileService(id);
+			console.log(response.data);
+			setAvatar(response.data.avatar);
+			setFullname(response.data.fullname);
+			setEmail(response.data.email);
+			setProfession(response.data.profession);
+			setAge(response.data.age);
+			setGender(response.data.gender);
+			setCity(response.data.city);
+			setCountry(response.data.country);
+		} catch (error) {
+			navigate('/error');
+		}
+	};
+
+	const handleEdit = async () => {
+		const profileObj = {
+			avatar,
+			fullname,
+			email,
+			profession,
+			age,
+			gender,
+			city,
+			country,
+		};
+
+		try {
+			await updateProfileService(id, profileObj);
+			navigate(`/profile/${id}`);
+		} catch (error) {
+			navigate('/error');
+		}
+	};
 
 	return (
 		<div className='profile--edit'>
@@ -27,6 +80,7 @@ function ProfileEdit() {
 
 			<div className='profile--edit__container'>
 				<div className='profile--edit__avatar'>
+					<label>Upload your photo profile</label>
 					<input
 						type='text'
 						name='avatar'
@@ -34,15 +88,8 @@ function ProfileEdit() {
 						onChange={handleAvatarChange}
 					/>
 				</div>
-				<div className='profile--edit__avatar'>
-					<input
-						type='text'
-						name='fullname'
-						value={fullname}
-						onChange={handleFullnameChange}
-					/>
-				</div>
 				<div className='profile--edit__fullname'>
+					<label>Full Name</label>
 					<input
 						type='text'
 						name='fullname'
@@ -51,6 +98,7 @@ function ProfileEdit() {
 					/>
 				</div>
 				<div className='profile--edit__email'>
+					<label>Email</label>
 					<input
 						type='text'
 						name='email'
@@ -59,6 +107,7 @@ function ProfileEdit() {
 					/>
 				</div>
 				<div className='profile--edit__profession'>
+					<label>Profession</label>
 					<input
 						type='text'
 						name='profession'
@@ -67,6 +116,7 @@ function ProfileEdit() {
 					/>
 				</div>
 				<div className='profile--edit__age'>
+					<label>Age</label>
 					<input
 						type='number'
 						name='age'
@@ -75,14 +125,22 @@ function ProfileEdit() {
 					/>
 				</div>
 				<div className='profile--edit__gender'>
-					<input
+					<label>Gender</label>
+					<select
 						type='text'
 						name='gender'
 						value={gender}
 						onChange={handleGenderChange}
-					/>
+					>
+						{arrGender.map(({ gender }) => (
+							<option value={gender} key={gender}>
+								{gender}
+							</option>
+						))}
+					</select>
 				</div>
 				<div className='profile--edit__city'>
+					<label>City</label>
 					<input
 						type='text'
 						name='city'
@@ -91,14 +149,17 @@ function ProfileEdit() {
 					/>
 				</div>
 				<div className='profile--edit__country'>
-					<input
-						type='text'
-						name='country'
-						value={country}
-						onChange={handleCountryChange}
-					/>
+					<label>Country</label>
+					<select name='country' value={country} onChange={handleCountryChange}>
+						{arrCountry.map(({ country }) => (
+							<option value={country} key={country}>
+								{country}
+							</option>
+						))}
+					</select>
 				</div>
 			</div>
+			<button onClick={handleEdit}>Update</button>
 		</div>
 	);
 }
